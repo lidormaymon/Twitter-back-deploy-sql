7ab7177ff9449a3efe187f8bb1b7fed46ec33f0e
lidormaymon <127669520+lidormaymon@users.noreply.github.com>
2023-09-29 15:23:44 +0300

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -33,6 +33,11 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "littermanaagement@gmail.com"
 EMAIL_HOST_PASSWORD = "xzbp lvla fjoy yyss"
 
+ALLOWED_HOSTS = [
+    "twitter-clone-backend-sqlite.onrender.com"
+]
+
+
 CORS_ORIGIN_ALLOW_ALL = True  # Allow requests from any origin
 
 
