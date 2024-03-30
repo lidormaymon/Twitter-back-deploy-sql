1df04394eac98f870313740df8b1409772ba08b7
lidor <lidorm20@gmail.com>
2024-03-30 19:28:13 +0300

    update

--- myproj/settings.py
+++ myproj/settings.py
@@ -38,15 +38,16 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "littermanaagement@gmail.com"
 EMAIL_HOST_PASSWORD = "xzbp lvla fjoy yyss"
 
-ALLOWED_HOSTS = ["twitter-clone-backend-sqlite.onrender.com"]
 
-
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "https://twitter-clone-backend-sqlite.onrender.com",
+    "127.0.0.1",
+]
 
 
 # Application definition
 
-SITE_ID = 1
+SITE_ID = 2
 
 INSTALLED_APPS = [
     "channels",
